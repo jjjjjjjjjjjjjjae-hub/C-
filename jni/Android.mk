269480feee2ba903db2af18196ebc08cc40a0bd0
Almas <almas@example.com>
2026-04-21 20:49:14 +0500

    Step 1: Fix C++ libraries and STL

--- jni/Android.mk
+++ jni/Android.mk
@@ -3,5 +3,6 @@ include $(CLEAR_VARS)
 
 LOCAL_MODULE    := ghost_cheat
 LOCAL_SRC_FILES := main.cpp
+LOCAL_LDLIBS    := -llog -landroid
 
 include $(BUILD_SHARED_LIBRARY)
--- jni/Application.mk
+++ jni/Application.mk
@@ -1,2 +1,3 @@
 APP_ABI := all
 APP_PLATFORM := android-21
+APP_STL := c++_shared
